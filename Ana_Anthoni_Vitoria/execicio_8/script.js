e57dacd0e3dedb67c4001e6c899a24545eec1a64
AnthOnnii <125268054+AnthOnnii@users.noreply.github.com>
2023-06-13 22:17:31 -0300

    Atualizar o script.js

--- Ana_Anthoni_Vitoria/execicio_8/script.js
+++ Ana_Anthoni_Vitoria/execicio_8/script.js
@@ -17,3 +17,22 @@ function capturarTerceiroNumeroInteiro() {
   const terceiro_numero_inteiro = Number(document.getElementById("terceiro_numero_inteiro").value); 
   return terceiro_numero_inteiro;
  }
+ function capturarQuartoNumeroInteiro() {
+  const quarto_numero_inteiro = Number(document.getElementById("quarto_numero_inteiro").value); 
+  return quarto_numero_inteiro;
+ }
+
+
+function somar() {
+  let primeiro_numero_inteiro = capturarPrimeiroNumeroInteiro();
+  let segundo_numero_inteiro = capturarSegundoNumeroInteiro();
+  let terceiro_numero_inteiro = capturarTerceiroNumeroInteiro();
+  let quarto_numero_inteiro = capturarQuartoNumeroInteiro ();
+  let resultado_media = (primeiro_numero_inteiro + segundo_numero_inteiro + terceiro_numero_inteiro + quarto_numero_inteiro)/4;
+  imprimirResultadoNaTela(resultado_media);
+}
+
+
+function imprimirResultadoNaTela(resultado_media) {
+  document.getElementById("resultado_media").value = resultado_media;
+}
